ba45b40f55757be514249a4347b7ed677844abc5
Aynur <a.nurytdinov@gmail.com>
2020-05-28 17:44:26 +0300

    add pages

--- src/app/tabs/profile-tab/profile-tab.module.ts
+++ src/app/tabs/profile-tab/profile-tab.module.ts
@@ -7,6 +7,10 @@ import { ProfileTabPage } from './profile-tab.page';
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 import { NotificationsPage } from './notifications/notifications.page';
 import { SettingsPage } from './settings/settings.page';
+import { StatsPage } from './stats/stats.page';
+import { AchievementsPage } from './achievements/achievements.page';
+import { PurchasesPage } from './purchases/purchases.page';
+import { ShopPage } from './shop/shop.page';
 
 @NgModule({
   imports: [
@@ -18,12 +22,20 @@ import { SettingsPage } from './settings/settings.page';
       { path: '', component: ProfileTabPage },
       { path: 'notifications', component: NotificationsPage },
       { path: 'settings', component: SettingsPage },
+      { path: 'stats', component: StatsPage },
+      { path: 'achievements', component: AchievementsPage },
+      { path: 'purchases', component: PurchasesPage },
+      { path: 'shop', component: ShopPage },
     ])
   ],
   declarations: [
     ProfileTabPage,
     NotificationsPage,
-    SettingsPage
+    SettingsPage,
+    StatsPage,
+    AchievementsPage,
+    PurchasesPage,
+    ShopPage
   ]
 })
 export class ProfileTabPageModule {}
