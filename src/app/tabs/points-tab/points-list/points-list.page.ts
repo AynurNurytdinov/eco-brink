5e67f771ade0b381d90b4e4cb6a309643d1acc16
Aynur <a.nurytdinov@gmail.com>
2020-05-30 23:23:12 +0300

    points list

--- src/app/tabs/points-tab/points-list/points-list.page.ts
+++ src/app/tabs/points-tab/points-list/points-list.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FilterModalPage } from '../filter-modal/filter-modal.page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-points-list',
@@ -6,7 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['points-list.page.scss']
 })
 export class PointsListPage {
-  constructor() {}
+  points = '/assets/data/points.geojson';
+  items;
+  constructor(
+    private http: HttpClient,
+    public modalController: ModalController
+  ) {}
+
+  ionViewDidEnter() {
+    this.load();
+  }
+
+  load() {
+    this.http.get(this.points).subscribe((res: any) => {
+      this.items = res.source.data.features;
+    });
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: FilterModalPage
+    });
+    return await modal.present();
+  }
 }
 
 
